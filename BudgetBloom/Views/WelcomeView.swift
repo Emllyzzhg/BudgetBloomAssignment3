a85e91296e4da3c028428d6daecd7b08707d0872
Hyunashi <163243070+Hyunashi@users.noreply.github.com>
2026-05-08 20:07:56 +1000

    added leaf and fixed padding on welcome page

--- BudgetBloom/Views/WelcomeView.swift
+++ BudgetBloom/Views/WelcomeView.swift
@@ -11,6 +11,12 @@ struct WelcomeView: View {
     
     var body: some View {
         VStack (alignment: .center) {
+            
+            Image(systemName: "leaf.fill")
+                .font(.system(size: 80))
+                .foregroundColor(.green)
+                .padding()
+            
             Text("Welcome to Budget Bloom")
             NavigationLink("Start") {
                 LoginView()
